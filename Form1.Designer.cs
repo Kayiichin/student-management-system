cc1af9cff93349fd5e8e1203e93df197ab403205
Kaye Bacos Bearneza <124229077+Kayiichin@users.noreply.github.com>
2023-03-23 00:01:22 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace Act_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.yearsec = new System.Windows.Forms.Label();
+            this.subject = new System.Windows.Forms.Label();
+            this.name_txt = new System.Windows.Forms.TextBox();
+            this.yearsec_txt = new System.Windows.Forms.TextBox();
+            this.sub_txt = new System.Windows.Forms.TextBox();
+            this.gr1 = new System.Windows.Forms.Label();
+            this.gr1_txt = new System.Windows.Forms.TextBox();
+            this.gr2 = new System.Windows.Forms.Label();
+            this.gr2_txt = new System.Windows.Forms.TextBox();
+            this.gr3 = new System.Windows.Forms.Label();
+            this.gr3_txt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(227, -16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(302, 212);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU-ExtB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(276, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Calculate Grade";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(79, 259);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(35, 13);
+            this.name.TabIndex = 2;
+            this.name.Text = "Name";
+            // 
+            // yearsec
+            // 
+            this.yearsec.AutoSize = true;
+            this.yearsec.Location = new System.Drawing.Point(79, 312);
+            this.yearsec.Name = "yearsec";
+            this.yearsec.Size = new System.Drawing.Size(89, 13);
+            this.yearsec.TabIndex = 3;
+            this.yearsec.Text = "Year and Section";
+            // 
+            // subject
+            // 
+            this.subject.AutoSize = true;
+            this.subject.Location = new System.Drawing.Point(79, 363);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(43, 13);
+            this.subject.TabIndex = 4;
+            this.subject.Text = "Subject";
+            // 
+            // name_txt
+            // 
+            this.name_txt.Location = new System.Drawing.Point(82, 275);
+            this.name_txt.Name = "name_txt";
+            this.name_txt.Size = new System.Drawing.Size(179, 20);
+            this.name_txt.TabIndex = 5;
+            // 
+            // yearsec_txt
+            // 
+            this.yearsec_txt.Location = new System.Drawing.Point(82, 328);
+            this.yearsec_txt.Name = "yearsec_txt";
+            this.yearsec_txt.Size = new System.Drawing.Size(179, 20);
+            this.yearsec_txt.TabIndex = 6;
+            // 
+            // sub_txt
+            // 
+            this.sub_txt.Location = new System.Drawing.Point(82, 379);
+            this.sub_txt.Name = "sub_txt";
+            this.sub_txt.Size = new System.Drawing.Size(179, 20);
+            this.sub_txt.TabIndex = 7;
+            // 
+            // gr1
+            // 
+            this.gr1.AutoSize = true;
+            this.gr1.Location = new System.Drawing.Point(450, 259);
+            this.gr1.Name = "gr1";
+            this.gr1.Size = new System.Drawing.Size(61, 13);
+            this.gr1.TabIndex = 8;
+            this.gr1.Text = "1st Grading";
+            // 
+            // gr1_txt
+            // 
+            this.gr1_txt.Location = new System.Drawing.Point(453, 275);
+            this.gr1_txt.Name = "gr1_txt";
+            this.gr1_txt.Size = new System.Drawing.Size(129, 20);
+            this.gr1_txt.TabIndex = 9;
+            // 
+            // gr2
+            // 
+            this.gr2.AutoSize = true;
+            this.gr2.Location = new System.Drawing.Point(450, 312);
+            this.gr2.Name = "gr2";
+            this.gr2.Size = new System.Drawing.Size(65, 13);
+            this.gr2.TabIndex = 10;
+            this.gr2.Text = "2nd Grading";
+            // 
+            // gr2_txt
+            // 
+            this.gr2_txt.Location = new System.Drawing.Point(453, 328);
+            this.gr2_txt.Name = "gr2_txt";
+            this.gr2_txt.Size = new System.Drawing.Size(129, 20);
+            this.gr2_txt.TabIndex = 11;
+            // 
+            // gr3
+            // 
+            this.gr3.AutoSize = true;
+            this.gr3.Location = new System.Drawing.Point(450, 363);
+            this.gr3.Name = "gr3";
+            this.gr3.Size = new System.Drawing.Size(62, 13);
+            this.gr3.TabIndex = 12;
+            this.gr3.Text = "3rd Grading";
+            // 
+            // gr3_txt
+            // 
+            this.gr3_txt.Location = new System.Drawing.Point(453, 379);
+            this.gr3_txt.Name = "gr3_txt";
+            this.gr3_txt.Size = new System.Drawing.Size(129, 20);
+            this.gr3_txt.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(290, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 32);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(730, 502);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gr3_txt);
+            this.Controls.Add(this.gr3);
+            this.Controls.Add(this.gr2_txt);
+            this.Controls.Add(this.gr2);
+            this.Controls.Add(this.gr1_txt);
+            this.Controls.Add(this.gr1);
+            this.Controls.Add(this.sub_txt);
+            this.Controls.Add(this.yearsec_txt);
+            this.Controls.Add(this.name_txt);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.yearsec);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label yearsec;
+        private System.Windows.Forms.Label subject;
+        private System.Windows.Forms.TextBox name_txt;
+        private System.Windows.Forms.TextBox yearsec_txt;
+        private System.Windows.Forms.TextBox sub_txt;
+        private System.Windows.Forms.Label gr1;
+        private System.Windows.Forms.TextBox gr1_txt;
+        private System.Windows.Forms.Label gr2;
+        private System.Windows.Forms.TextBox gr2_txt;
+        private System.Windows.Forms.Label gr3;
+        private System.Windows.Forms.TextBox gr3_txt;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Act_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void clearData()
+        {
+            name_txt.Text = "";
+            yearsec_txt.Text = "";
+            sub_txt.Text = "";
+            gr1.Text = "";
+            gr2.Text = "";
+            gr3.Text = "";
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionStr = "server = localhost;user=root;database=cit203;port3306;password=";
+
+            string name = name_txt.Text;
+            string yearsec = yearsec_txt.Text;
+            string subject = sub_txt.Text;
+            double g1 = Convert.ToInt32(gr1_txt.Text);
+            double g2 = Convert.ToInt32(gr2_txt.Text);
+            double g3 = Convert.ToInt32(gr3_txt.Text);
+
+            if (name == " " || yearsec == " " || subject == " " || g1 == 0 || g2 == 0 || g3 == 0)
+
+            {
+                MessageBox.Show("Please fill in the details asked.");
+            }
+
+            else
+            {
+
+                double totalgrade = (g1 + g2 + g3) / 3;
+
+                MySqlConnection conn = new MySqlConnection(connectionStr);
+                MySqlCommand cmd = null; 
+                string addcommandstring = "";
+                conn.Open();
+
+
+            
+
+                addcommandstring = "INSERT INTO  student (fullname, yrsec, subject, totalgrade) VALUES (@fullname, @yrsec, @subject, @totalgrade)";
+
+                cmd = new MySqlCommand(addcommandstring, conn);
+
+                cmd.Parameters.AddWithValue("@fullname", name);
+                cmd.Parameters.AddWithValue("@yrsec", yearsec);
+                cmd.Parameters.AddWithValue("@subject", subject);
+                cmd.Parameters.AddWithValue("@totalgrade", totalgrade);
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+
+            }
+            MessageBox.Show("Data Stored Successfully");
+            clearData();
+
+
+        }
+    }
+}
+ 
